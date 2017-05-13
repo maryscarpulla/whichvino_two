7f5f56f39513a49c79b37f16a07337a7c16ec7a1
Raghu Betina <raghu@firstdraft.com>
2017-05-13 16:38:16 +0000

    run rails g active_admin:install

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   root :to => "wines#index"
   # Routes for the My_wine resource:
   # CREATE
