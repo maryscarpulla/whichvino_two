9dd38afe7ba7af15e4d52d45fe45d61d0c9b2137
Raghu Betina <raghu@firstdraft.com>
2017-05-13 16:37:49 +0000

    Generate my_wines resource with starter:resource

---
+++ app/models/my_wine.rb
@@ -0,0 +1,8 @@
+class MyWine < ApplicationRecord
+  # Direct associations
+
+  # Indirect associations
+
+  # Validations
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+  # Routes for the My_wine resource:
+  # CREATE
+  get "/my_wines/new", :controller => "my_wines", :action => "new"
+  post "/create_my_wine", :controller => "my_wines", :action => "create"
+
+  # READ
+  get "/my_wines", :controller => "my_wines", :action => "index"
+  get "/my_wines/:id", :controller => "my_wines", :action => "show"
+
+  # UPDATE
+  get "/my_wines/:id/edit", :controller => "my_wines", :action => "edit"
+  post "/update_my_wine/:id", :controller => "my_wines", :action => "update"
+
+  # DELETE
+  get "/delete_my_wine/:id", :controller => "my_wines", :action => "destroy"
+  #------------------------------
+
   # Routes for the Wine resource:
   # CREATE
   get "/wines/new", :controller => "wines", :action => "new"
