ac5ab0da620879877a01490d607379aa604a5bb9
Mary Lin <marylin@chicagobooth.edu>
2017-05-23 13:40:18 -0500

    api works
    just need to figure out how to link "Add" button

--- app/controllers/wine_search_controller.rb
+++ app/controllers/wine_search_controller.rb
@@ -18,19 +18,7 @@ class WineSearchController < ApplicationController
     url = "http://api.snooth.com/wines/?akey=4ki3im2n32x688cqwnb8onkpuwmvxo9yi3pbz4qrtmd9nmwt&ip=66.28.234.115&q=#{@wine_search}&xp=30"
 
     parsed_data = JSON.parse(open(url).read)
-
-
-    @winename1 = parsed_data["wines"][0]["name"]
-    @winery1 = parsed_data["wines"][0]["winery"]
-    @varietal1 = parsed_data["wines"][0]["varietal"]
-    @year1 = parsed_data["wines"][0]["vintage"]
-    @image1 = parsed_data["wines"][0]["image"]
-
-    @winename2 = parsed_data["wines"][1]["name"]
-    @winery2 = parsed_data["wines"][1]["winery"]
-    @varietal2 = parsed_data["wines"][1]["varietal"]
-    @year2 = parsed_data["wines"][1]["vintage"]
-    @image2 = parsed_data["wines"][1]["image"]
+    @wines_from_api = parsed_data["wines"]
 
     render("wine_search_results.html.erb")
 
